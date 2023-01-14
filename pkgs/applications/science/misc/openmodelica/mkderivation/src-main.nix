f3d2f6dbdc9f1e93444cd8d1160f30f9e5ea557b
ppenguin <hieronymusv@gmail.com>
2023-01-14 12:26:21 +0100

    openmodelica: 1.20 hash

--- pkgs/applications/science/misc/openmodelica/mkderivation/src-main.nix
+++ pkgs/applications/science/misc/openmodelica/mkderivation/src-main.nix
@@ -1,7 +1,7 @@
 {
   url = "https://github.com/OpenModelica/OpenModelica/";
-  rev = "49be4faa5a625a18efbbd74cc2f5be86aeea37bb";
-  sha256 = "0klqiy4sdizl1djb9hb0arcvfcjz2mmnakrjx81mmxcbr8yq2016";
+  rev = "d42a0e2a9dbff9d75a280b0ed9c60c0d39a35621";
+  sha256 = "04l7dgr2h6ggp2wbb2xdynq981qm2l74vxkgb4b74sq06r7l2dwv";
   fetchSubmodules = true;
 }
 # Update with: nix run -f ./nixpkgs/default.nix nix-prefetch-git -c nix-prefetch-git 'https://github.com/OpenModelica/OpenModelica/' 'v1.18.0' --fetch-submodules
